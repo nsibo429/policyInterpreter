fff4ad3a85d0f24a4347dc98a802680b5d1af941
nsibo429 <nsibo@uwo.ca>
2020-04-25 21:18:53 -0400

    regex added

--- app/src/main/java/com/example/policyinterpreter/MainActivity.java
+++ app/src/main/java/com/example/policyinterpreter/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     Button analyzeButton;
     String results;
+    String resultsD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(getApplicationContext(), ResultsActivity.class);
                 intent.putExtra("results", results);
+                intent.putExtra("resultsD", resultsD);
                 startActivity(intent);
                 finish();
             }
@@ -62,19 +64,30 @@ public class MainActivity extends AppCompatActivity {
         }
         is.close();
         results = MatchPolicy(stringBuilder.toString());
+        resultsD = DisclosureMatchPolicy(stringBuilder.toString());
         //Toast.makeText(getBaseContext(), results,
           //      Toast.LENGTH_LONG).show();
     }
 
     public String MatchPolicy(String policyData){
-        Pattern pattern = Pattern.compile("[^.]* collect [^.]*\\.");
+        Pattern pattern = Pattern.compile("[^\\.]* collect [^\\.]*.");
         Matcher matcher = pattern.matcher(policyData);
         if (matcher.find())
         {
-            Log.d("collect pattern",matcher.group());
+            Log.d("pattern",matcher.group());
         }
 
         return matcher.group();
     }
 
+    public String DisclosureMatchPolicy(String policyData){
+        Pattern pattern = Pattern.compile("[^\\.]* disclose [^\\.]*.");
+        Matcher matcher = pattern.matcher(policyData);
+        if (matcher.find())
+        {
+            Log.d("pattern",matcher.group());
+        }
+
+        return matcher.group();
+    }
 }
--- app/src/main/java/com/example/policyinterpreter/ResultsActivity.java
+++ app/src/main/java/com/example/policyinterpreter/ResultsActivity.java
@@ -1,6 +1,7 @@
 package com.example.policyinterpreter;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ResultsActivity extends AppCompatActivity {
 
     TextView resultsView;
+    TextView resultsDView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +17,14 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
         Bundle bundle = getIntent().getExtras();
         String results = bundle.getString("results");
+        String resultsD = bundle.getString("resultsD");
         resultsView = findViewById(R.id.resultsView);
+        resultsDView = findViewById(R.id.resultsDView);
+        resultsView.setMovementMethod(new ScrollingMovementMethod());
+        resultsDView.setMovementMethod(new ScrollingMovementMethod());
         resultsView.setText(results);
+        resultsDView.setText(resultsD);
+
 
     }
 }
